8a64949f543fc956eee43280d72c72ea9788b1aa
nhatprovklno3 <nguyenhongnhat71298@gmail.com>
2021-03-15 08:34:45 +0700

    Chapter 10: Updating, showing and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  before_action :logged_in_user, except: [:new, :create]
+  before_action :find_user_on_params, except: [:index, :new, :create]
+  before_action :correct_user, only: [:edit, :update]
 
-    flash[:warning] = t "warning.find_by_id"
-    redirect_to root_path
+  def edit; end
+
+  def show; end
+
+  def index
+    @users = User.ordered_by_name_desc.paginate(page: params[:page],
+                                           per_page: Settings.paginate.per_page)
   end
 
   def new
@@ -22,10 +27,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update user_params
+      flash[:success] = t "warning.edit_succes"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    name_delected = @user.name
+    if current_user.admin && @user.destroy
+      flash[:success] = t("warning.delete_succes", user_name: name_delected)
+    else
+      flash[:danger] = t "warning.delete_error"
+    end
+    redirect_to users_url
+  end
+
   private
 
   def user_params
     params.require(:user).permit :name, :email,
                                  :password, :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "warning.please_login"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def find_user_on_params
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "warning.find_by_id"
+    redirect_to root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,21 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
-  before_save ->{email.downcase!}
+
+  has_secure_password
+
   validates :name, presence: true,
                    length: {maximum: Settings.user.name.max_length}
   validates :email, presence: true,
                     length: {maximum: Settings.user.email.max_length},
                     format: {with: Settings.user.email.regex},
                     uniqueness: true
-  has_secure_password
   validates :password, presence: true,
-                       length: {minimum: Settings.user.password.min_length}
+                       length: {minimum: Settings.user.password.min_length},
+                       allow_nil: Settings.user.allow_nil
+
+  before_save ->{email.downcase!}
+
+  scope :ordered_by_name_desc, ->{order(name: :desc)}
 
   def self.digest string
     cost =  if ActiveModel::SecurePassword.min_cost
